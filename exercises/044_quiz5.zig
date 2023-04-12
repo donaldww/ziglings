26deb0043c64c90d2508fcc4413a91790718448d
Donald Wilson <donaldww@icepointmedia.ca>
2023-04-12 13:42:45 -0400

    Add 039 to 044

--- exercises/044_quiz5.zig
+++ exercises/044_quiz5.zig
@@ -13,18 +13,20 @@ const std = @import("std");
 const Elephant = struct {
     letter: u8,
     tail: *Elephant = undefined,
-    visited: bool = false,
+    visited: bool = false
 };
 
 pub fn main() void {
     var elephantA = Elephant{ .letter = 'A' };
     // (Please add Elephant B here!)
+    var elephantB = Elephant{ .letter = 'B' };
     var elephantC = Elephant{ .letter = 'C' };
 
     // Link the elephants so that each tail "points" to the next elephant.
     // They make a circle: A->B->C->A...
     elephantA.tail = &elephantB;
     // (Please link Elephant B's tail to Elephant C here!)
+    elephantB.tail = &elephantC;
     elephantC.tail = &elephantA;
 
     visitElephants(&elephantA);
